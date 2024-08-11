3ccc68eb4032a55bc3a8757363d93bc912cc38ee
Simon VIZIOLI <simon.vizioli@irrigacion.gov.ar>
2024-08-11 16:24:22 -0300

    modified routing and base

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import Contact from "./components/Resume/Contact";
 
 const router = createBrowserRouter([
     {
-        path: "/resuem",
+        path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
