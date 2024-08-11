3e69a073f3b1fb1e2d6e3521f3862742eec51d1c
Simon VIZIOLI <simon.vizioli@irrigacion.gov.ar>
2024-08-11 16:28:20 -0300

    modified routing and base

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import Contact from "./components/Resume/Contact";
 
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/resume",
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig(() => {
     return {
         plugins: [react()],
-        base: "/resume/",
+        base: "/resume",
         resolve: {
             alias: {
                 "@": path.resolve(__dirname, "./src"),
