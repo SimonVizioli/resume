53e48294ece460bd68912bb0fcdd0acd8612848c
Simón Vizioli <111660358+SimonVizioli@users.noreply.github.com>
2024-08-13 15:55:44 -0300

    Update card.tsx

--- src/components/Resume/Contact/card.tsx
+++ src/components/Resume/Contact/card.tsx
@@ -17,7 +17,7 @@ const ContactCard = (props: ContactCardProps) => {
         <Link
             to={link}
             target="_blank"
-            className="transition ease-in-out delay-50 bg-blue-950 text-white shadow-lg rounded-xl p-8 lg:p-10 flex flex-col items-center space-y-4 lg:space-y-10 transform hover:scale-105 transition-transform duration-300 dark:text-gray-800 dark:hover:text-teal-500 dark:bg-white"
+            className="transition ease-in-out delay-50 bg-blue-950 text-white shadow-lg rounded-xl p-8 lg:p-10 flex flex-col items-center space-y-8 lg:space-y-10 transform hover:scale-105 transition-transform duration-300 dark:text-gray-800 dark:hover:text-teal-500 dark:bg-white"
         >
             <div className="w-16 h-16 flex items-center justify-center bg-teal-100 text-teal-600 rounded-full">
                 {/* Aquí podrías usar un icono SVG */}
